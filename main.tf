1f2a645f46b8613bf9317e011e0647d1202527a7
yedukondalu-mcd <89128208+yedukondalu-mcd@users.noreply.github.com>
2021-08-18 15:41:30 +0530

    Update and rename test.tf to main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,9 @@
 provider "aws" {
    region      =  "us-east-1"
-   #access_key  =  "${ secrets.AWS_ACCESS_KEY_ID }"
-   #secret_key  =  "${ secrets.AWS_SECRET_ACCESS_KEY }"
 }
 
 resource "aws_instance" "my-first-server" {
-   ami           = "ami-0c2b8ca1dad447f8a"
+   ami           = "ami-09e67e426f25ce0d7"
    instance_type = "t2.micro"
    tags = {
       Name = "Ubuntu"
